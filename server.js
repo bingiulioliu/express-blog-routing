077dcf715105ee694ef97a8246d2ef33ff13fdcd
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-22 16:02:06 +0200

    ADD listen

--- server.js
+++ server.js
@@ -5,3 +5,14 @@ const app=express();
 const PORT= process.env.PORT;
 const URL= process.env.URL;
 
+
+
+
+app.listen(PORT, (error) => {
+    if(error) {
+        console.error(error);
+        return;
+    }
+    console.log(`Server avviato alla porta ${PORT}`);
+    
+})
