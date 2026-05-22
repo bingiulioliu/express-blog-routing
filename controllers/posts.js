09879bc1b3d65f0815e592c850a9016a64ca2313
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-22 16:34:01 +0200

    FEAT validazione in posts/:id aggiunta

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,7 +5,47 @@ function index (request, response) {
 }
 
 function show (request, response) {
-    response.json(posts)
+    // Recuper l'ID dai params
+    const {id} = request.params;
+
+    const realId = Number(id.trim())
+
+    if (isNaN(realId)) {
+        response.status(404)
+        .json({
+            error: 'ID non corretto',
+            result: null
+        });
+        return;
+    }
+
+    if (realId<=0 || realId > 5){
+        response.status(404)
+        .json({
+            error: 'ID minore di zero o maggiore di 5',
+            result: null
+        });
+        return;
+    }
+
+    const postFound = posts.find(post => {
+        return post.id === realId
+    });
+
+    if (postFound === undefined) {
+        response.status(404)
+        .json({
+            error: 'Post non trovato',
+            result: null
+        });
+        return;
+    }
+
+    response.json({
+        error: null,
+        results: postFound
+    });
+
 }
 
 function create (request, response) {
